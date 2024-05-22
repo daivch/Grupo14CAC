f12f4ab0976320ab23c5e1d8ba92b1d410cb1c23
Lucaslancioni <166434357+Lucaslancioni@users.noreply.github.com>
2024-05-22 18:15:38 -0300

    Update pedido.js

--- formulario/pedido.js
+++ formulario/pedido.js
@@ -1,7 +1,4 @@
-document.getElementById('formularioTorta').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y la página se recargue
-    validarFormulario();
-});
+
 
 function validarFormulario() {
    
